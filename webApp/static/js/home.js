3436a252b04623b44cff2cfe12858888be7b1e78
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2019-04-24 10:10:45 -0300

    Se pueden ver eventos de cada usuario.

--- webApp/static/js/home.js
+++ webApp/static/js/home.js
@@ -62,6 +62,31 @@ function crearEvento(){
     request.send(event);
 }
 
+
+function showEventsFromUser(){
+    const url = "http://localhost:8080/events/fromUser";
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('Authorization', 'Bearer ' + window.sessionStorage.token);
+    request.setRequestHeader('Accept', 'application/json');
+    request.send();
+    request.onload = () => {
+        const htmlList = document.getElementById("events-from-user");
+        while(htmlList.firstChild){
+            htmlList.removeChild(htmlList.firstChild)
+        }
+        const eventList = JSON.parse(request.response);
+        for (let i = 0; i < eventList.length; i++) {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.innerText = eventList[i].name;
+            li.appendChild(a);
+            htmlList.appendChild(li);
+        }
+    }
+}
+
 function buscarEvento() {
     const name = document.getElementById("evento-buscado").value;
     const url = "http://localhost:8080/events/" + name;
